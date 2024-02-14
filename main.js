3e159097dce4586963c1ecbaf094198a49481ee6
suyosuyo <149132459+suyosuyo@users.noreply.github.com>
2024-02-14 13:52:07 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// スライダー
+
+// var windowWidth = $(window).width();
+
 $("#main-slider").slick({
     //ここにオプション
     asNavFor:"#text-slider", //テキストスライダーを追従させる
@@ -5,7 +9,15 @@ $("#main-slider").slick({
     centerMode:true,        //追記
     centerPadding:"20%",    //追記
     variableWidth: true // スライド幅の自動計算を無効化
+    
 })
+
+// if(windowWidth < 700) {
+//     $("#main-slider").slick({
+//     arrows:false           //矢印非表示
+//     })
+// }
+
 $("#text-slider").slick({
     //ここにオプション
     draggable:false,         //ドラッグでのスライド禁止
@@ -15,3 +27,17 @@ $("#text-slider").slick({
     variableWidth: true, // スライド幅の自動計算を無効化
     dots:true
 })
+
+
+// ナビゲーション
+$(function () {
+    $('#navbutton').click(() => {
+      $('#navres').toggleClass('nav-active');
+      $('#navbutton').toggleClass('button-active');
+    });
+  
+    $('#nav-menu').click(() => {
+        $('#navres').removeClass('nav-active');
+        $('#navbutton').removeClass('button-active');
+    });
+  });
